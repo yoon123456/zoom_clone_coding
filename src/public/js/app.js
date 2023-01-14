3ba8ddd30d5a8ffbeb52e51898dbfa6d448883c3
yoon123456 <yw01124@gmail.com>
2023-01-14 19:28:55 +0900

    👍소켓 연결 및 메세지 보내기

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,18 @@
-alert("hi");
+// 서버로의 연결 socket
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () => {
+  console.log("conneted to server!! ✅");
+});
+
+socket.addEventListener("message", (msg) => {
+  console.log("new message", msg.data, "from server");
+});
+
+socket.addEventListener("close", () => {
+  console.log("disconneted from server!! ❌");
+});
+
+setTimeout(() => {
+  socket.send("hello from the browser");
+}, 5000);
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket, { WebSocketServer } from "ws";
 import express from "express";
 
 const app = express();
@@ -6,6 +8,23 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server });
+
+// 브라우저로의 연결 socket
+wss.on("connection", (socket) => {
+  socket.send("hello");
+  socket.on("close", () => {
+    console.log("disconneted from browser!! ❌");
+  });
+  socket.on("message", (msg) => {
+    console.log(msg.toString("utf8"));
+  });
+  console.log("conneted to browser!! ✅");
+});
+
+server.listen(3000, handleListen);
